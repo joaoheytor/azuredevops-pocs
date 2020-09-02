6171f46048563cd66d5d8097eac167438f7b2e0e
João Heytor Kreitlow Pereira <jhkpereira@gmail.com>
2020-09-01 22:05:43 -0300

    Adding new methods

--- vaultmgmt.py
+++ vaultmgmt.py
@@ -1,17 +1,47 @@
-from azure.core.exceptions import ResourceNotFoundError, ClientAuthenticationError
 from azure.identity import ClientSecretCredential
+from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+
 
 class AzureVault:
     
-    def add_secret():
+    def add_secret(self):
         """
-        Responsible to ADD secrets in vault.
+        Responsible to ADD secrets in Azure Vault.
+        ref.: https://pypi.org/project/azure-keyvault-secrets/#set-a-secret
         """
-        print("TBD")
+        credential = DefaultAzureCredential()
+        # credential = ClientSecretCredential(tenant_id, client_id, client_secret)
 
-    def delete_secret():
+        secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
+        secret = secret_client.set_secret("secret-name", "secret-value")
+
+        print(secret.name)
+        print(secret.value)
+        print(secret.properties.version)
+
+    def delete_secret(self):
         """
         Responsible to DELETE secrets in vault.
+        ref.: https://pypi.org/project/azure-keyvault-secrets/#delete-a-secret
         """
-        print("TBD")
+        credential = DefaultAzureCredential()
+
+        secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
+        deleted_secret = secret_client.begin_delete_secret("secret-name").result()
+
+        print(deleted_secret.name)
+        print(deleted_secret.deleted_date)
+
+    def get_secret(self):
+        """
+        Responsible to RETRIEVE secrets from Vault.
+        ref.: https://pypi.org/project/azure-keyvault-secrets/#retrieve-a-secret
+        """
+        credential = DefaultAzureCredential()
+
+        secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
+        secret = secret_client.get_secret("secret-name")
+
+        print(secret.name)
+        print(secret.value)
