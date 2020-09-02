d9c248086d2b16863b2157c4cdc66fcafe38da0a
João Heytor Kreitlow Pereira <jhkpereira@gmail.com>
2020-09-01 21:51:36 -0300

    Initial idea

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # azuredevops-pocs
 Useful scripts to use within Azure DevOps
+
+# Intro
+The scripts available here were used to study some APIs and resources available within the Azure
---
+++ vaultmgmt.py
@@ -0,0 +1,17 @@
+from azure.core.exceptions import ResourceNotFoundError, ClientAuthenticationError
+from azure.identity import ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+
+class AzureVault:
+    
+    def add_secret():
+        """
+        Responsible to ADD secrets in vault.
+        """
+        print("TBD")
+
+    def delete_secret():
+        """
+        Responsible to DELETE secrets in vault.
+        """
+        print("TBD")
